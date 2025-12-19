553df61afd819bd72c06641064fef1a8e52be6be
andreisanta <dey_san@yahoo.com>
2025-12-19 09:24:14 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const menuContainer = document.getElementById('menu');
+const filterButtons = document.querySelectorAll('.filters button');
+
+let cocktails = [];
+
+// Încarcă JSON-ul extern
+fetch('drinks.json')
+    .then(res => res.json())
+    .then(data => {
+        cocktails = data;
+        renderMenu('all');
+    })
+    .catch(err => console.error("Eroare la citirea drinks.json:", err));
+
+function renderMenu(filter = 'all') {
+    menuContainer.innerHTML = '';
+    let filtered = cocktails;
+    if (filter !== 'all') {
+        filtered = cocktails.filter(c => c.category === filter);
+    }
+
+    filtered.forEach(c => {
+        const ingredients = c.ingredients.map(i => i.item).join(', ')
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `
+      <div class="image-container">
+         <img src="${c.image}" alt="${c.name}" onerror="this.onerror=null; this.src='images/not_found.png';">
+      </div>
+      <h2>${c.name}</h2>
+      <p class="content">${ingredients}</p>
+      <a href="reteta.html?id=${c.id}" class="btn">Vezi rețeta</a>
+    `;
+        menuContainer.appendChild(card);
+    });
+}
+
+// ===== Filter buttons =====
+filterButtons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        filterButtons.forEach(b => b.classList.remove('active'));
+        btn.classList.add('active');
+        renderMenu(btn.dataset.filter);
+    });
+});
